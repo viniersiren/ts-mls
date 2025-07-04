367d21832e2ac809a3e052364311d471a72dd3d9
ljacobowitz <luka.jacobowitz@gmail.com>
2025-07-04 18:52:51 -0400

    1.0.0

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,46 @@
-export { createGroup, makePskIndex, joinGroup } from "./clientState"
+export { type Extension, type ExtensionType } from "./extension"
+
+export { defaultProposalTypes, type DefaultProposalTypeName } from "./defaultProposalType"
+
+export { defaultExtensionTypes, type DefaultExtensionTypeName } from "./defaultExtensionType"
+
+export { type PrivateKeyPath } from "./privateKeyPath"
+
+export { type RatchetTree } from "./ratchetTree"
+
+export { acceptAll, type IncomingMessageCallback, type IncomingMessageAction } from "./IncomingMessageAction"
+
+export { proposeAddExternal, proposeExternal } from "./externalProposal"
+
+export { type GroupContext } from "./groupContext"
+
+export { decodeExternalSender, encodeExternalSender, type ExternalSender } from "./externalSender"
+
+export {
+  decodeRequiredCapabilities,
+  encodeRequiredCapabilities,
+  type RequiredCapabilities,
+} from "./requiredCapabilities"
+
+export { type AuthenticationService, defaultAuthenticationService } from "./authenticationService"
+
+export { type PaddingConfig, defaultPaddingConfig } from "./paddingConfig"
+
+export { defaultKeyPackageEqualityConfig, type KeyPackageEqualityConfig } from "./keyPackageEqualityConfig"
+
+export { type LifetimeConfig, defaultLifetimeConfig } from "./lifetimeConfig"
+
+export { type PrivateKeyPackage, type KeyPackage, generateKeyPackage } from "./keyPackage"
+export { type KeyRetentionConfig, defaultKeyRetentionConfig } from "./keyRetentionConfig"
+
+export {
+  createGroup,
+  makePskIndex,
+  joinGroup,
+  type ClientState,
+  type GroupActiveState,
+  type EpochReceiverData,
+} from "./clientState"
 
 export { createApplicationMessage, createProposal } from "./createMessage"
 
@@ -7,9 +49,15 @@ export {
   createCommit,
   createGroupInfoWithExternalPub,
   createGroupInfoWithExternalPubAndRatchetTree,
+  type CreateCommitResult,
 } from "./createCommit"
 
-export { processPrivateMessage, processPublicMessage } from "./processMessages"
+export {
+  processPrivateMessage,
+  processMessage,
+  processPublicMessage,
+  type ProcessMessageResult,
+} from "./processMessages"
 
 export { type PskIndex, emptyPskIndex } from "./pskIndex"
 
@@ -19,13 +67,21 @@ export { type Credential } from "./credential"
 
 export { type Proposal } from "./proposal"
 
-export { type CiphersuiteName, ciphersuites, getCiphersuiteFromName, getCiphersuiteImpl } from "./crypto/ciphersuite"
+export { type ClientConfig } from "./clientConfig"
+
+export { type Welcome } from "./welcome"
+
+export {
+  type CiphersuiteName,
+  type CiphersuiteImpl,
+  ciphersuites,
+  getCiphersuiteFromName,
+  getCiphersuiteImpl,
+} from "./crypto/ciphersuite"
 
 export { bytesToBase64 } from "./util/byteArray"
 
-export { generateKeyPackage } from "./keyPackage"
 export { decodeMlsMessage, encodeMlsMessage } from "./message"
-export { type ProposalAdd } from "./proposal"
 export { type Lifetime, defaultLifetime } from "./lifetime"
 export { type Capabilities } from "./capabilities"
 export { defaultCapabilities } from "./defaultCapabilities"
